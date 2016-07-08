d1c13a896a639d620d5fa2a86d3fb7c1bf97289e
einhornschaf <sofia.lampe@tum.de>
2016-07-08 15:16:17 +0200

    neue Rezepte

--- Assets/Scripts/Rezept.cs
+++ Assets/Scripts/Rezept.cs
@@ -14,7 +14,9 @@ public class Rezept
     public enum ID
     {
         Tomatensalat,
-        Bratkartoffeln
+        Bratkartoffeln,
+        Curry_mit_Suesskartoffeln_und_Brokkoli,
+        Pasta_mit_Brokkoli_Pilz_Sosse
         //Weitere Rezepte durch Komma getrennt einfuegen
     }
 
@@ -45,6 +47,19 @@ public class Rezept
             //2. Zutaten mit beliebiger Menge auffuellen. Bitte jede Zutat nur einmalig. Die mitgelieferte Zahl ist die Anzahl
             //3. Rezept erstellen mit Zutaten, Kochanleitung
             //4. Zutaten leeren und von vorn das Ganze
+
+            zutaten.Add(new Tupel(Zutat.ID.Suesskartoffel, 2));
+            zutaten.Add(new Tupel(Zutat.ID.Brokkoli, 5));
+            rezepte.Add(new Rezept(ID.Curry_mit_Suesskartoffeln_und_Brokkoli, zutaten, "Zutaten: \n2 große Süßkartoffel \n2 Zwiebeln \n2 Knoblauchzehen \n1 Stück Ingwer \n4 EL Butter oder Öl \nCurry, Curcuma, Kreuzkümmel, Cayennepfeffer, Koriander, Sojasauce \n400 ml Kokosmilch \n300 ml Brühe \n500 g Brokkoli \n300g Reis \n\nZubereitung: \nBrokkoli waschen und in mundgerechte Röschen teilen.\nZwiebel, Ingwer und Knoblauchzehe fein hacken.Süßkartoffel schälen und in kleine Würfel zuschneiden.In einer großen Pfanne die Zwiebel anschwitzen, Knoblauch, Ingwer und Gewürze nur kurz erhitzen. \nDie Süßkartoffel dazu geben und einige Minuten mit dünsten.Anschließend mit der Kokosmilch ablöschen und die Brühe hinzugeben. 5 Minuten köcheln lassen. \nIn der Zwischenzeit den Reis nach Packungsanweisung kochen. \nBrokkoli dazu geben und für weitere 10 Minuten zugedeckt köcheln lassen.Mit der Sojsauce, Cayennepfeffer Salz und Koriandergrün abschmecken."));
+            zutaten.Clear();
+
+            zutaten.Add(new Tupel(Zutat.ID.Brokkoli, 5));
+            zutaten.Add(new Tupel(Zutat.ID.Pilz, 3));
+            zutaten.Add(new Tupel(Zutat.ID.Tomate, 4));
+            rezepte.Add(new Rezept(ID.Pasta_mit_Brokkoli_Pilz_Sosse, zutaten, "Zutaten: \n500g Brokkoli \n300g Champignons \n1 kleine Zwiebel \n1,5 - 2 Dosen Tomaten stückig \n1 - 2 Knoblauchzehen \n100 ml Schlagsahne \n300g - 500g Nudeln \nfrischer Basilikum \nOlivenöl, Salz, Pfeffer,geriebener Parmesan zum Bestreuen \n\nZubereitung: \nZwiebel abziehen, klein hacken. Champignons in Streifen schneiden. Brokkoli in kleine Röschen schneiden, in kochendem Wasser 4 - 5 Minuten garen. Zwiebel in Olivenöl andünsten, Knoblauch zugeben.Die Champignons braten bis zu braun werden.Dann Tomaten, und Sahne zugeben.Nudeln mit zwei Esslöffeln Olivenöl bissfest kochen. Mit Salz, Pfeffer und Basilikum abschmecken. Am Tisch: Parmesan"));
+            zutaten.Clear();
+
+
         }
         return rezepte;
     }
@@ -54,7 +69,15 @@ public class Rezept
     */
     public static string getName(ID id)
     {
-        return id.ToString();
+        switch (id)
+        {
+            case ID.Curry_mit_Suesskartoffeln_und_Brokkoli:
+                return "Curry mit Süßkartoffeln und Brokkoli";
+            case ID.Pasta_mit_Brokkoli_Pilz_Sosse:
+                return "Pasta mit Brokkoli-Pilz-Soße";
+            default:
+                return id.ToString();
+        }
     }
 
     /*
